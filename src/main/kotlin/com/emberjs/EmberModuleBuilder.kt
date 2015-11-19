2b4dda3be1d9c468533fdf492275670225189bca
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-19 09:21:09 +0100

    ModuleBuilder: Use Ember.js favicon for "New Project" dialog

--- src/main/kotlin/com/emberjs/EmberModuleBuilder.kt
+++ src/main/kotlin/com/emberjs/EmberModuleBuilder.kt
@@ -2,8 +2,11 @@ package com.emberjs
 
 import com.intellij.ide.util.projectWizard.ModuleBuilder
 import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.util.IconLoader
 
 class EmberModuleBuilder : ModuleBuilder() {
+
+    override fun getNodeIcon() = IconLoader.getIcon("/com/emberjs/module.png")
 
     override fun setupRootModel(modifiableRootModel: ModifiableRootModel) {
     }
