c7712e73b70a4d2d1d7ef9c9499a91391a278a68
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-19 18:27:33 +0100

    Port Ember.String.classify() to Kotlin

---
+++ src/main/kotlin/com/emberjs/utils/EmberStringUtils.kt
@@ -0,0 +1,27 @@
+package com.emberjs.utils
+
+import kotlin.text.MatchResult
+import kotlin.text.Regex
+
+private val STRING_CLASSIFY_REGEXP_1 = Regex("^(\\-|_)+(.)?");
+private val STRING_CLASSIFY_REGEXP_2 = Regex("(.)(\\-|\\_|\\.|\\s)+(.)?");
+private val STRING_CLASSIFY_REGEXP_3 = Regex("(^|\\/|\\.)([a-z])");
+
+public fun String.classify(): String {
+    val replace1: (MatchResult) -> String = {
+        it.groups[2]?.value?.toUpperCase()?.prepend("_").orEmpty()
+    }
+
+    val replace2: (MatchResult) -> String = {
+        it.groups[3]?.value?.toUpperCase().orEmpty().prepend(it.groups[1]?.value.orEmpty())
+    }
+
+    return this.splitToSequence("/")
+            .map { part -> part.replace(STRING_CLASSIFY_REGEXP_1, replace1) }
+            .map { part -> part.replace(STRING_CLASSIFY_REGEXP_2, replace2) }
+            .joinToString("/")
+            .replace(STRING_CLASSIFY_REGEXP_3) { it.value.toUpperCase() }
+}
+
+public fun String.prepend(prefix: String) = "$prefix${this}"
+public fun String.append(suffix: String) = "${this}$suffix"
