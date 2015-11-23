77be309d2079329f33d95adc886fb6eedfad1d65
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-23 22:43:23 +0100

    index: Use EmberName class

--- src/main/kotlin/com/emberjs/index/EmberIndexBase.kt
+++ src/main/kotlin/com/emberjs/index/EmberIndexBase.kt
@@ -1,9 +1,9 @@
 package com.emberjs.index
 
+import com.emberjs.resolver.EmberName
 import com.emberjs.utils.VoidHelper
-import com.emberjs.utils.append
-import com.emberjs.utils.classify
 import com.emberjs.utils.parents
+import com.intellij.openapi.project.ProjectLocator
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.indexing.*
 import com.intellij.util.io.EnumeratorStringDescriptor
@@ -13,46 +13,33 @@ abstract class EmberIndexBase(private val name: ID<String, Void>) :
         FileBasedIndex.InputFilter,
         DataIndexer<String, Void, FileContent> {
 
-    val appFolder: String = name.toString().removePrefix("ember.").append("s")
-    val suffix: String = name.toString().removePrefix("ember.").capitalize()
-
     override fun getName() = name
-    override fun getVersion() = 1
+    override fun getVersion() = 2
     override fun getKeyDescriptor() = KEY_DESCRIPTIOR
     override fun dependsOnFileContent() = false
 
     override fun getInputFilter() = this
-    override fun acceptInput(file: VirtualFile) =
-            file.extension == "js" && file.inAppFolder(appFolder)
+    override fun acceptInput(file: VirtualFile): Boolean {
+        val project = ProjectLocator.getInstance().guessProjectForFile(file) ?: return false
+        val appFolder = project.baseDir.findChild("app") ?: return false
+
+        return (file.extension == "js" || file.extension == "hbs") && file.parents.any { it == appFolder }
+    }
 
     override fun getIndexer() = this
     override fun map(inputData: FileContent): Map<String, Void> {
 
-        // Convert path to class name
-        val name = inputData.file.toClassName(appFolder).append(suffix)
+        val file = inputData.file
+        val project = ProjectLocator.getInstance().guessProjectForFile(file) ?: return mapOf()
+        val name = EmberName.from(project.baseDir, file) ?: return mapOf()
 
-        return mapOf(Pair(name, VoidHelper.get()))
+        if (name.type != this.name.toString().removePrefix("ember."))
+            return mapOf()
+
+        return mapOf(Pair(name.displayName, VoidHelper.get()))
     }
 
     companion object {
         val KEY_DESCRIPTIOR = EnumeratorStringDescriptor()
-
-        /**
-         * Converts path to class name
-         */
-        fun VirtualFile.toClassName(appFolder: String): String =
-                sequence(this, { it.parent })
-                        .map { it.nameWithoutExtension }
-                        .takeWhile { it != appFolder }
-                        .toList()
-                        .asReversed()
-                        .map { it.classify() }
-                        .joinToString("")
-
-        private fun VirtualFile.getParentDirectory(dirName: String) =
-                parents.firstOrNull { it.name == dirName }
-
-        private fun VirtualFile.inAppFolder(dirName: String) =
-                getParentDirectory(dirName)?.parent?.name == "app"
     }
 }
