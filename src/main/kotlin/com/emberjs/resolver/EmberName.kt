06af5dc0f4a1411cda6c36060a7beb62da87954e
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-23 20:53:16 +0100

    EmberName: Implement VirtualFile -> EmberName converter

--- src/main/kotlin/com/emberjs/resolver/EmberName.kt
+++ src/main/kotlin/com/emberjs/resolver/EmberName.kt
@@ -1,16 +1,65 @@
 package com.emberjs.resolver
 
+import com.emberjs.EmberFileType
+import com.emberjs.utils.parents
+import com.intellij.openapi.vfs.VirtualFile
+
 data class EmberName private constructor(val type: String, val name: String) {
 
     val fullName: String
         get() = "$type:$name"
 
     companion object {
+        private val KNOWN_TYPES = EmberFileType.values.filter { it != EmberFileType.COMPONENT_TEMPLATE }
+
         fun from(fullName: String): EmberName? {
             val parts = fullName.split(":")
             return when {
                 parts.count() == 2 -> EmberName(parts[0], parts[1])
                 else -> null
+            }
+        }
+
+        fun from(root: VirtualFile, file: VirtualFile): EmberName? {
+            val appFolder = root.findChild("app") ?: return null
+
+            return fromPod(root, appFolder, file) ?:
+                    fromClassic(appFolder, file)
+        }
+
+        fun fromClassic(appFolder: VirtualFile, file: VirtualFile): EmberName? {
+            val typeFolder = file.parents.find { it.parent == appFolder } ?: return null
+
+            return KNOWN_TYPES.find { it.folderName == typeFolder.name }?.let { type ->
+
+                var path = file.parents
+                        .takeWhile { it != typeFolder }
+                        .map { it.name }
+                        .reversed()
+                        .joinToString("/")
+
+                val name = "$path/${file.nameWithoutExtension}".removePrefix("/")
+
+                EmberName(type.name.toLowerCase(), name)
+            }
+        }
+
+        fun fromPod(root: VirtualFile, appFolder: VirtualFile, file: VirtualFile): EmberName? {
+            return KNOWN_TYPES.find { it.fileName == file.name }?.let { type ->
+
+                var name = file.parents
+                        .takeWhile { it != appFolder && it != root }
+                        .map { it.name }
+                        .reversed()
+                        .joinToString("/")
+                        .let {
+                            when (type) {
+                                EmberFileType.COMPONENT -> it.removePrefix("components/")
+                                else -> it
+                            }
+                        }
+
+                EmberName(type.name.toLowerCase(), name)
             }
         }
     }
--- src/test/kotlin/com/emberjs/resolver/EmberNameTest.kt
+++ src/test/kotlin/com/emberjs/resolver/EmberNameTest.kt
@@ -1,6 +1,13 @@
 package com.emberjs.resolver
 
+import com.emberjs.EmberTestFixtures.APTIBLE
+import com.emberjs.EmberTestFixtures.CRATES_IO
+import com.emberjs.EmberTestFixtures.EXAMPLE
+import com.emberjs.utils.find
+import com.emberjs.utils.use
+import com.intellij.openapi.vfs.VirtualFile
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions
 import org.junit.Test
 
 class EmberNameTest {
@@ -15,5 +22,51 @@ class EmberNameTest {
     @Test fun testInvalidName() {
         val name = EmberName.from("foobar")
         assertThat(name).isNull()
+    }
+
+    @Test fun testCratesIo() = doTest(CRATES_IO, mapOf(
+            Pair("app/adapters/application.js", "adapter:application"),
+            Pair("app/components/crate-row.js", "component:crate-row"),
+            Pair("app/controllers/keyword/index.js", "controller:keyword/index"),
+            Pair("app/helpers/format-num.js", "helper:format-num"),
+            //Pair("app/initializers/google.js", "initializer:google"),
+            //Pair("app/mixins/pagination.js", "mixin:pagination"),
+            Pair("app/models/crate.js", "model:crate"),
+            Pair("app/routes/crate/index.js", "route:crate/index"),
+            Pair("app/serializers/crate.js", "serializer:crate"),
+            Pair("app/services/session.js", "service:session"),
+            Pair("app/templates/application.hbs", "template:application"),
+            Pair("app/templates/components/crate-row.hbs", "template:components/crate-row")
+    ))
+
+    @Test fun testExample() = doTest(EXAMPLE, mapOf(
+            Pair("app/application/adapter.js", "adapter:application"),
+            Pair("app/pet/model.js", "model:pet"),
+            Pair("app/pet/serializer.js", "serializer:pet"),
+            Pair("app/session/service.js", "service:session"),
+            Pair("app/application/template.hbs", "template:application"),
+            Pair("app/user/adapter.js", "adapter:user"),
+            Pair("app/user/model.js", "model:user")
+    ))
+
+    @Test fun testAptible() = doTest(APTIBLE, mapOf(
+            Pair("app/app/controller.js", "controller:app"),
+            Pair("app/app/route.js", "route:app"),
+            Pair("app/app/template.hbs", "template:app"),
+            //Pair("app/app/view.js", "view:app"),
+            Pair("app/app/vhosts/route.js", "route:app/vhosts"),
+            Pair("app/app/vhosts/template.hbs", "template:app/vhosts"),
+            Pair("app/components/change-plan/component.js", "component:change-plan"),
+            Pair("app/components/change-plan/template.hbs", "template:components/change-plan")
+    ))
+
+    private fun doTest(root: VirtualFile, tests: Map<String, String?>) {
+        SoftAssertions().use {
+            for ((path, expectedName) in tests) {
+                assertThat(EmberName.from(root, root.find(path))?.fullName)
+                        .describedAs(path)
+                        .isEqualTo(expectedName)
+            }
+        }
     }
 }
