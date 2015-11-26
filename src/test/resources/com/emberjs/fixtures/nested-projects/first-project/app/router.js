c6c4bffbf57910c80628d8afe9e33457247b57ac
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-26 18:19:55 +0100

    test: Add an example project with two nested modules

---
+++ src/test/resources/com/emberjs/fixtures/nested-projects/first-project/app/router.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route('pets');
+  this.route('pet');
+});
+
+export default Router;
