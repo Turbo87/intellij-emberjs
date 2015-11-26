9c1e505064eba182ec3f6117ca465bc051143065
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-11-26 18:51:46 +0100

    FileIndex: Use module roots for indexing

--- src/main/kotlin/com/emberjs/index/EmberFileIndex.kt
+++ src/main/kotlin/com/emberjs/index/EmberFileIndex.kt
@@ -1,8 +1,12 @@
 package com.emberjs.index
 
+import com.emberjs.project.EmberModuleType
 import com.emberjs.resolver.EmberName
 import com.emberjs.utils.VoidHelper
+import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.ProjectLocator
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.indexing.*
 import com.intellij.util.io.EnumeratorStringDescriptor
@@ -13,7 +17,7 @@ class EmberFileIndex() :
         DataIndexer<String, Void, FileContent> {
 
     override fun getName() = NAME
-    override fun getVersion() = 1
+    override fun getVersion() = 2
     override fun getKeyDescriptor() = KEY_DESCRIPTIOR
     override fun dependsOnFileContent() = false
 
@@ -25,9 +29,15 @@ class EmberFileIndex() :
     override fun map(inputData: FileContent): Map<String, Void> {
         val file = inputData.file
         val project = ProjectLocator.getInstance().guessProjectForFile(file) ?: return mapOf()
-        val name = EmberName.from(project, file) ?: return mapOf()
+        val module = ModuleUtilCore.findModuleForFile(file, project) ?: return mapOf()
 
-        return mapOf(Pair(name.displayName, VoidHelper.get()))
+        if (ModuleType.get(module) !is EmberModuleType)
+            return mapOf()
+
+        return ModuleRootManager.getInstance(module).contentRoots
+                .map { EmberName.from(it, file) }
+                .filterNotNull()
+                .toMap({ it.displayName }, { VoidHelper.get() })
     }
 
     companion object {
