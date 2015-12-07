631799293769574e1b84c3bbf3f615d5c9545ac0
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-12-07 22:18:29 +0100

    actions, cli: Use EmberProjectComponent class

--- src/main/kotlin/com/emberjs/actions/EmberGenerateCodeAction.kt
+++ src/main/kotlin/com/emberjs/actions/EmberGenerateCodeAction.kt
@@ -3,9 +3,9 @@ package com.emberjs.actions
 import com.emberjs.cli.EmberCli
 import com.emberjs.cli.EmberCliGenerateTask
 import com.emberjs.icons.EmberIcons
-import com.emberjs.utils.getEmberModule
+import com.emberjs.utils.emberRoot
 import com.emberjs.utils.getIdeView
-import com.emberjs.utils.hasEmberModule
+import com.emberjs.utils.hasEmberRoot
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.DumbAwareAction
@@ -14,11 +14,12 @@ class EmberGenerateCodeAction() :
         DumbAwareAction("Ember.js Files", "Generates new Ember.js files via ember-cli", EmberIcons.ICON_16) {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val module = e.getEmberModule() ?: return
+        val project = e.project ?: return
+        val workDir = e.emberRoot ?: return
         val view = e.getIdeView()
 
         // Prepare blueprint chooser dialog
-        val dialog = EmberGenerateCodeDialog(module.project).apply {
+        val dialog = EmberGenerateCodeDialog(project).apply {
             EmberCli.BLUEPRINTS.forEach { addBlueprint(it.key, it.value) }
             selectedTemplate = DEFAULT_BLUEPRINT
         }
@@ -32,11 +33,11 @@ class EmberGenerateCodeAction() :
         FileDocumentManager.getInstance().saveAllDocuments()
 
         // Run ember-cli as modal task
-        EmberCliGenerateTask(module, dialog.selectedTemplate, dialog.enteredName, view).queue()
+        EmberCliGenerateTask(project, workDir, dialog.selectedTemplate, dialog.enteredName, view).queue()
     }
 
     override fun update(e: AnActionEvent) {
-        e.presentation.isEnabledAndVisible = e.hasEmberModule()
+        e.presentation.isEnabledAndVisible = e.hasEmberRoot
     }
 
     companion object {
