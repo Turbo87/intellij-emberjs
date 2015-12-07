d22a2ac89bbd7f19bb6c2fd52a69d85f77520375
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-12-07 19:39:03 +0100

    index: Use EmberProjectComponent class

--- src/main/kotlin/com/emberjs/index/EmberNameIndex.kt
+++ src/main/kotlin/com/emberjs/index/EmberNameIndex.kt
@@ -1,15 +1,11 @@
 package com.emberjs.index
 
+import com.emberjs.project.EmberProjectComponent
 import com.emberjs.resolver.EmberName
-import com.emberjs.utils.getEmberModule
 import com.emberjs.utils.guessProject
-import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.vfs.VfsUtil.isAncestor
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.indexing.*
-import com.intellij.util.io.IOUtil
-import com.intellij.util.io.KeyDescriptor
-import java.io.DataInput
-import java.io.DataOutput
 
 class EmberNameIndex() :
         ScalarIndexExtension<EmberName>(),
@@ -17,7 +13,7 @@ class EmberNameIndex() :
         DataIndexer<EmberName, Void?, FileContent> {
 
     override fun getName() = NAME
-    override fun getVersion() = 1
+    override fun getVersion() = 2
     override fun getKeyDescriptor() = KEY_DESCRIPTIOR
     override fun dependsOnFileContent() = false
 
@@ -29,9 +25,9 @@ class EmberNameIndex() :
     override fun map(inputData: FileContent): Map<EmberName, Void?> {
         val file = inputData.file
         val project = file.guessProject() ?: return mapOf()
-        val module = file.getEmberModule(project) ?: return mapOf()
+        val roots = EmberProjectComponent.getInstance(project)?.roots ?: return mapOf()
 
-        return ModuleRootManager.getInstance(module).contentRoots
+        return roots.filter { isAncestor(it, file, true) }
                 .map { EmberName.from(it, file) }
                 .filterNotNull()
                 .toMap({ it }, { null })
