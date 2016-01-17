ab287639d13914e3405a1348c90d126f101ebcab
Tobias Bieniek <tobias.bieniek@gmx.de>
2016-01-17 15:07:31 +0100

    Replace arrayOf() with emptyArray()

--- src/main/kotlin/com/emberjs/template/EmberLiveTemplatesProvider.kt
+++ src/main/kotlin/com/emberjs/template/EmberLiveTemplatesProvider.kt
@@ -4,7 +4,7 @@ import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider
 
 class EmberLiveTemplatesProvider : DefaultLiveTemplatesProvider {
     override fun getDefaultLiveTemplateFiles() = DEFAULT_TEMPLATES
-    override fun getHiddenLiveTemplateFiles() = arrayOf<String>()
+    override fun getHiddenLiveTemplateFiles() = emptyArray<String>()
 
     companion object {
         val DEFAULT_TEMPLATES = arrayOf("/com/emberjs/template/Ember.js", "/com/emberjs/template/HTMLBars");
