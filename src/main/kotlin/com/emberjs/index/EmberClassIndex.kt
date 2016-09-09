0bed2891825c7399db39d7f84ff707ce2474c292
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-09 20:56:28 +0200

    EmberClassIndex: Simplify code

--- src/main/kotlin/com/emberjs/index/EmberClassIndex.kt
+++ src/main/kotlin/com/emberjs/index/EmberClassIndex.kt
@@ -4,31 +4,26 @@ import com.emberjs.project.EmberProjectComponent
 import com.emberjs.resolver.EmberName
 import com.emberjs.utils.guessProject
 import com.intellij.openapi.vfs.VfsUtil.isAncestor
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.indexing.*
 import com.intellij.util.io.EnumeratorStringDescriptor
 
-class EmberClassIndex() :
-        ScalarIndexExtension<String>(),
-        FileBasedIndex.InputFilter,
-        DataIndexer<String, Void?, FileContent> {
+class EmberClassIndex() : ScalarIndexExtension<String>() {
 
     override fun getName() = NAME
     override fun getVersion() = 3
     override fun getKeyDescriptor() = KEY_DESCRIPTIOR
     override fun dependsOnFileContent() = false
 
-    override fun getInputFilter() = this
-    override fun acceptInput(file: VirtualFile) =
-            file.extension == "js" || file.extension == "hbs" || file.extension == "handlebars"
+    override fun getInputFilter() = FileBasedIndex.InputFilter {
+        it.extension == "js" || it.extension == "hbs" || it.extension == "handlebars"
+    }
 
-    override fun getIndexer() = this
-    override fun map(inputData: FileContent): Map<String, Void?> {
+    override fun getIndexer() = DataIndexer<String, Void?, FileContent> { inputData ->
         val file = inputData.file
-        val project = file.guessProject() ?: return mapOf()
-        val roots = EmberProjectComponent.getInstance(project)?.roots ?: return mapOf()
+        val project = file.guessProject() ?: return@DataIndexer mapOf()
+        val roots = EmberProjectComponent.getInstance(project)?.roots ?: return@DataIndexer mapOf()
 
-        return roots.filter { isAncestor(it, file, true) }
+        roots.filter { isAncestor(it, file, true) }
                 .map { EmberName.from(it, file) }
                 .filterNotNull()
                 .associateBy({ it.displayName }, { null })
