aa5ffe65fa4499a55801574199b374d695fc741f
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-13 00:43:44 +0200

    HbsPatterns: Simplify patterns

--- src/main/kotlin/com/emberjs/hbs/HbsCompletionContributor.kt
+++ src/main/kotlin/com/emberjs/hbs/HbsCompletionContributor.kt
@@ -1,8 +1,8 @@
 package com.emberjs.hbs
 
-import com.emberjs.hbs.HbsPatterns.BLOCK_MUSTACHE_NAME_PATTERN
-import com.emberjs.hbs.HbsPatterns.SIMPLE_MUSTACHE_NAME_PATTERN
-import com.emberjs.hbs.HbsPatterns.SUB_EXPR_NAME_PATTERN
+import com.emberjs.hbs.HbsPatterns.BLOCK_MUSTACHE_NAME_ID
+import com.emberjs.hbs.HbsPatterns.SIMPLE_MUSTACHE_NAME_ID
+import com.emberjs.hbs.HbsPatterns.SUB_EXPR_NAME_ID
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
 
@@ -13,20 +13,20 @@ import com.intellij.codeInsight.completion.CompletionType
  */
 class HbsCompletionContributor : CompletionContributor() {
     init {
-        extend(CompletionType.BASIC, SIMPLE_MUSTACHE_NAME_PATTERN, HbsComponentCompletionProvider())
-        extend(CompletionType.BASIC, BLOCK_MUSTACHE_NAME_PATTERN, HbsComponentCompletionProvider())
+        extend(CompletionType.BASIC, SIMPLE_MUSTACHE_NAME_ID, HbsComponentCompletionProvider())
+        extend(CompletionType.BASIC, BLOCK_MUSTACHE_NAME_ID, HbsComponentCompletionProvider())
 
-        extend(CompletionType.BASIC, SIMPLE_MUSTACHE_NAME_PATTERN, HbsHelperCompletionProvider())
-        extend(CompletionType.BASIC, SUB_EXPR_NAME_PATTERN, HbsHelperCompletionProvider())
+        extend(CompletionType.BASIC, SIMPLE_MUSTACHE_NAME_ID, HbsHelperCompletionProvider())
+        extend(CompletionType.BASIC, SUB_EXPR_NAME_ID, HbsHelperCompletionProvider())
 
-        extend(CompletionType.BASIC, SIMPLE_MUSTACHE_NAME_PATTERN, HbsBuiltinHelperCompletionProvider(
+        extend(CompletionType.BASIC, SIMPLE_MUSTACHE_NAME_ID, HbsBuiltinHelperCompletionProvider(
                 "action", "component", "debugger", "get", "if", "input", "link-to", "loc", "log",
                 "outlet", "partial", "render", "textarea", "unbound"))
 
-        extend(CompletionType.BASIC, BLOCK_MUSTACHE_NAME_PATTERN, HbsBuiltinHelperCompletionProvider(
+        extend(CompletionType.BASIC, BLOCK_MUSTACHE_NAME_ID, HbsBuiltinHelperCompletionProvider(
                 "component", "each", "each-in", "if", "link-to", "unless", "with"))
 
-        extend(CompletionType.BASIC, SUB_EXPR_NAME_PATTERN, HbsBuiltinHelperCompletionProvider(
+        extend(CompletionType.BASIC, SUB_EXPR_NAME_ID, HbsBuiltinHelperCompletionProvider(
                 "action", "component", "concat", "get", "hash", "if", "loc", "mut", "query-params",
                 "unbound", "unless"))
     }
