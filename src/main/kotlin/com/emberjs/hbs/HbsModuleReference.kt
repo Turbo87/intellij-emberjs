d0c16c3f7e32b577de534a9722e5e3a08e38f599
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-13 01:25:29 +0200

    HbsModuleReference: Use index in multiResolve()

--- src/main/kotlin/com/emberjs/hbs/HbsModuleReference.kt
+++ src/main/kotlin/com/emberjs/hbs/HbsModuleReference.kt
@@ -3,40 +3,42 @@ package com.emberjs.hbs
 import com.dmarcotte.handlebars.psi.HbMustacheName
 import com.emberjs.index.EmberNameIndex
 import com.emberjs.lookup.EmberLookupElementBuilder
-import com.emberjs.project.EmberProjectComponent
 import com.emberjs.resolver.EmberName
-import com.emberjs.resolver.EmberResolver
 import com.intellij.openapi.util.Condition
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.*
+import com.intellij.psi.PsiElementResolveResult.createResults
+import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiPolyVariantReferenceBase
+import com.intellij.psi.ResolveResult
 import com.intellij.util.CommonProcessors
 import com.intellij.util.FilteringProcessor
 import com.intellij.util.indexing.FileBasedIndex
 import com.intellij.util.indexing.FindSymbolParameters
-import com.intellij.psi.PsiElementResolveResult.createResults
+
 class HbsModuleReference(element: HbMustacheName, val moduleType: String) :
         PsiPolyVariantReferenceBase<HbMustacheName>(element, TextRange(0, element.textLength), true) {
 
     override fun multiResolve(incompleteCode: Boolean): Array<out ResolveResult> {
-        val rootsSeq = EmberProjectComponent.getInstance(element.project)?.roots?.asSequence() ?: return emptyArray()
 
+        val scope = FindSymbolParameters.searchScopeFor(element.project, true)
+
+        val collector = CommonProcessors.CollectProcessor<EmberName>()
+        val filter = FilteringProcessor<EmberName>(Condition { it.type == moduleType && it.name == value }, collector)
+
+        val index = FileBasedIndex.getInstance()
         val psiManager = PsiManager.getInstance(element.project)
 
-        // Iterate over Ember.js roots of the project
-        return createResults(rootsSeq.flatMap {
-            val resolver = EmberResolver(it)
+        // Collect all components from the index
+        index.processAllKeys(EmberNameIndex.NAME, filter, scope, null)
 
-            // Look for type with matching name in root folder
-            sequenceOf(value, value.removeSuffix("s"))
-                    .map { resolver.resolve("$moduleType:$it") }
-                    .distinct()
-        }
-                .filterNotNull()
+        return createResults(collector.results
 
-                // Convert VirtualFile to PsiFile
+                // Filter out components that are not related to this project
+                .flatMap { index.getContainingFiles(EmberNameIndex.NAME, it, scope) }
+
+                // Convert search results for LookupElements
                 .map { psiManager.findFile(it) }
-                .filterNotNull()
-                .toList())
+                .filterNotNull())
     }
 
     override fun getVariants(): Array<out Any?> {
