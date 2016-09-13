9c8c8986bf1f92c4be0b8461dee45a434c581273
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-13 21:35:15 +0200

    EmberJSLiteralReference: Use EmberNameIndex for resolve() method

--- src/main/kotlin/com/emberjs/psi/EmberJSLiteralReference.kt
+++ src/main/kotlin/com/emberjs/psi/EmberJSLiteralReference.kt
@@ -2,20 +2,21 @@ package com.emberjs.psi
 
 import com.emberjs.index.EmberNameIndex
 import com.emberjs.lookup.EmberLookupElementBuilder
-import com.emberjs.project.EmberProjectComponent
-import com.emberjs.resolver.EmberResolver
 import com.intellij.lang.javascript.psi.JSLiteralExpression
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult.createResults
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiPolyVariantReferenceBase
 import com.intellij.psi.ResolveResult
-import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.ProjectScope
 
 class EmberJSLiteralReference(element: JSLiteralExpression, val types: Iterable<String>) :
         PsiPolyVariantReferenceBase<JSLiteralExpression>(element, true) {
 
     val project = element.project
+    private val scope = ProjectScope.getAllScope(project)
+
+    private val psiManager: PsiManager by lazy { PsiManager.getInstance(project) }
 
     override fun multiResolve(incompleteCode: Boolean): Array<out ResolveResult> {
         val value = element.value
@@ -26,35 +27,20 @@ class EmberJSLiteralReference(element: JSLiteralExpression, val types: Iterable<
     }
 
     private fun resolve(value: String): Collection<PsiElement> {
-        val rootsSeq = EmberProjectComponent.getInstance(project)?.roots?.asSequence() ?: return listOf()
+        val names = arrayOf(value, value.removeSuffix("s"))
 
-        val psiManager = PsiManager.getInstance(project)
+        // Collect all matching modules from the index
+        return EmberNameIndex.getFilteredKeys(scope) { types.contains(it.type) && names.contains(it.name) }
 
-        // Iterate over types that we are looking for (e.g. "model" and "adapter")
-        return types.asSequence()
+                // Filter out components that are not related to this project
+                .flatMap { EmberNameIndex.getContainingFiles(it, scope) }
 
-                // Additionally iterate over Ember.js roots of the project
-                .flatMap { type ->
-                    rootsSeq.flatMap {
-                        val resolver = EmberResolver(it)
-
-                        // Look for type with matching name in root folder
-                        sequenceOf(value, value.removeSuffix("s"))
-                                .map { resolver.resolve("$type:$it") }
-                                .distinct()
-                    }
-                }
-                .filterNotNull()
-
-                // Convert VirtualFile to PsiFile
+                // Lookup corresponding PsiFiles
                 .map { psiManager.findFile(it) }
                 .filterNotNull()
-                .toList()
     }
 
     override fun getVariants(): Array<out Any> {
-        val scope = GlobalSearchScope.projectScope(project)
-
         return EmberNameIndex.getFilteredKeys(scope) { it.type == types.firstOrNull() }
 
                 // Filter out modules that are not related to this project
