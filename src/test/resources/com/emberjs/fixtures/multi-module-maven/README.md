6a3305585989edd28ce3c82f73e7cc5d29829db1
Derek Wickern <dwickern@gmail.com>
2016-10-28 08:54:24 -0700

    Add multi-module maven example

---
+++ src/test/resources/com/emberjs/fixtures/multi-module-maven/README.md
@@ -0,0 +1,7 @@
+Sample maven project
+===
+
+### Usage
+
+1. Import this directory using external model: Maven
+2. Click "next" through the wizard
---
+++ src/test/resources/com/emberjs/fixtures/multi-module-maven/java-module/src/main/java/com/github/dwickern/JavaMain.java
@@ -0,0 +1,10 @@
+package com.github.dwickern;
+
+/**
+ * Created by dwickern on 10/28/2016.
+ */
+public class JavaMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
