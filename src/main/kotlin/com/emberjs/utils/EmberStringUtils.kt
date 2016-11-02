6f6b8c491a4c5d7f3c82bda815dcf2c981bbf45e
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-11-02 18:32:59 +0100

    EmberStringUtils: Implement decamelize() extension function

--- src/main/kotlin/com/emberjs/utils/EmberStringUtils.kt
+++ src/main/kotlin/com/emberjs/utils/EmberStringUtils.kt
@@ -1,5 +1,6 @@
 package com.emberjs.utils
 
+import java.util.Locale
 import kotlin.text.MatchResult
 import kotlin.text.Regex
 
@@ -22,6 +23,10 @@ fun String.classify(): String {
             .joinToString("/")
             .replace(STRING_CLASSIFY_REGEXP_3) { it.value.toUpperCase() }
 }
+
+private val STRING_DECAMELIZE_REGEXP = Regex("([a-z\\d])([A-Z])")
+
+fun String.decamelize(): String = replace(STRING_DECAMELIZE_REGEXP, "$1_$2").toLowerCase(Locale.ROOT)
 
 fun String.prepend(prefix: String) = "$prefix${this}"
 fun String.append(suffix: String) = "${this}$suffix"
--- src/test/kotlin/com/emberjs/utils/EmberStringUtilsTest.kt
+++ src/test/kotlin/com/emberjs/utils/EmberStringUtilsTest.kt
@@ -21,7 +21,19 @@ class EmberStringUtilsTest {
     @Test fun `does nothing with classified string`() = classify("InnerHTML", "InnerHTML")
     @Test fun `does nothing with classified prefixed string`() = classify("_FooBar", "_FooBar")
 
+    @Test fun `decamelize does nothing with normal string`() = decamelize("my favorite items", "my favorite items")
+    @Test fun `decamelize does nothing with dasherized string`() = decamelize("css-class-name", "css-class-name")
+    @Test fun `decamelize does nothing with underscored string`() = decamelize("action_name", "action_name")
+    @Test fun `decamelize converts a camelized string into all lower case separated by underscores`() = decamelize("innerHTML", "inner_html")
+    @Test fun `decamelizes strings with numbers`() = decamelize("size160Url", "size160_url")
+    @Test fun `decamelize namespaced classified string`() = decamelize("PrivateDocs/OwnerInvoice", "private_docs/owner_invoice")
+    @Test fun `decamelize namespaced camelized string`() = decamelize("privateDocs/ownerInvoice", "private_docs/owner_invoice")
+
     private fun classify(input: String, expected: String) {
         assertThat(input.classify()).isEqualTo(expected)
+    }
+
+    private fun decamelize(input: String, expected: String) {
+        assertThat(input.decamelize()).isEqualTo(expected)
     }
 }
