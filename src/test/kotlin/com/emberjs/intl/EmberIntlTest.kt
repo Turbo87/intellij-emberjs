56af6f25a9b3791231ca38d08a1d25a955ce8333
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-11-06 21:19:57 +0100

    intl: Add EmberIntlTest class

---
+++ src/test/kotlin/com/emberjs/intl/EmberIntlTest.kt
@@ -0,0 +1,26 @@
+package com.emberjs.intl
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+import com.intellij.util.indexing.FileBasedIndex
+import org.assertj.core.api.Assertions.assertThat
+import java.nio.file.Paths
+
+class EmberIntlTest : LightPlatformCodeInsightFixtureTestCase() {
+
+    override fun getTestDataPath(): String? {
+        val resource = ClassLoader.getSystemResource("com/emberjs/intl/fixtures")
+        return Paths.get(resource.toURI()).toAbsolutePath().toString()
+    }
+
+    fun testFindBaseLocale() {
+        // Load fixture files into the project
+        myFixture.copyDirectoryToProject("ember-intl-with-base-locale", "/")
+
+        // Rebuild index now that the `package.json` file is copied over
+        FileBasedIndex.getInstance().requestRebuild(EmberIntlIndex.NAME)
+
+        val psiFile = myFixture.configureByFile("app/templates/base-locale-test.hbs")
+
+        assertThat(EmberIntl.findBaseLocale(psiFile)).isEqualTo("de")
+    }
+}
