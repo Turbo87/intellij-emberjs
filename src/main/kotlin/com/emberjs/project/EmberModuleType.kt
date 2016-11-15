5e21faf640abc7ec00c57e0199bb318c34b35fd1
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-11-15 13:48:03 +0100

    Recover EmberModuleType class

---
+++ src/main/kotlin/com/emberjs/project/EmberModuleType.kt
@@ -0,0 +1,30 @@
+package com.emberjs.project
+
+import com.emberjs.icons.EmberIcons
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.module.ModuleTypeManager
+import com.intellij.openapi.module.WebModuleType
+import javax.swing.Icon
+
+/**
+ * This class exists only for legacy reasons.
+ *
+ * @deprecated Use the `EmberCliFrameworkDetector` class instead!
+ */
+class EmberModuleType : WebModuleType() {
+
+    override fun getName() = NAME
+    override fun getDescription() = DESCRIPTION
+
+    override fun getNodeIcon(isOpened: Boolean): Icon = AllIcons.Nodes.Module
+    override fun getBigIcon(): Icon = EmberIcons.ICON_24
+
+    companion object {
+        val ID = "EMBER_MODULE"
+        val NAME = "Ember.js"
+        val DESCRIPTION = "Ember.js application module"
+
+        val instance: EmberModuleType
+            get() = ModuleTypeManager.getInstance().findByID(ID) as EmberModuleType
+    }
+}
