d94496230c2ca19584ad0f7a8e4a3f1e7a9f9e35
Derek Wickern <dwickern@gmail.com>
2017-07-14 12:16:13 -0700

    debug in-repo addons

--- src/main/kotlin/com/emberjs/resolver/EmberFileUrlMapper.kt
+++ src/main/kotlin/com/emberjs/resolver/EmberFileUrlMapper.kt
@@ -3,6 +3,7 @@ package com.emberjs.resolver
 import com.emberjs.Ember
 import com.emberjs.cli.EmberCliProjectConfigurator.Companion.inRepoAddons
 import com.emberjs.utils.emberRoot
+import com.emberjs.utils.parentEmberApp
 import com.emberjs.utils.parentEmberModule
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
@@ -15,19 +16,25 @@ class EmberFileUrlMapper : FileUrlMapper() {
     /**
      * Find the source file associated with the URL
      *
-     * For example:
+     * For example,
      *
-     *   http://localhost:4200/assets/my-app/components/foo.js => [
-     *     file://path/to/my-app/app/components/foo.js
-     *     file://path/to/my-app/app/lib/my-in-repo-addon/app/components/foo.js
-     *   ]
+     *   http://localhost:4200/assets/my-app/components/foo.js
+     *
+     * could resolve to either of these paths:
+     *
+     *   file://path/to/my-app/app/components/foo.js
+     *   file://path/to/my-app/app/lib/my-in-repo-addon/app/components/foo.js
+     *
      */
     override fun getFile(url: Url, project: Project, requestor: Url?): VirtualFile? {
         return ModuleManager.getInstance(project).modules.asSequence()
             .mapNotNull { it.emberRoot }
-            .flatMap { sequenceOf(it).plus(inRepoAddons(it)) }
-            .mapNotNull { root ->
-                Ember.getAppName(root)?.let { appName ->
+            .flatMap { app ->
+                val appName = Ember.getAppName(app) ?: return@flatMap emptySequence<VirtualFile>()
+
+                // search the app and in-repo addons
+                val roots = sequenceOf(app).plus(inRepoAddons(app))
+                roots.mapNotNull { root ->
                     val path = url.path.removePrefix("/assets/$appName/")
                     root.findFileByRelativePath("app/$path")
                 }
@@ -41,12 +48,14 @@ class EmberFileUrlMapper : FileUrlMapper() {
      * For example:
      *
      *   file://path/to/my-app/app/components/foo.js                    => http://localhost:4200/assets/my-app/components/foo.js
+     *   file://path/to/my-app/lib/in-repo-addon/app/components/foo.js  => http://localhost:4200/assets/my-app/components/foo.js
      *   file://path/to/my-app/tests/integration/components/foo-test.js => http://localhost:4200/assets/my-app/tests/integration/components/foo-test.js
      */
     override fun getUrls(file: VirtualFile, project: Project, currentAuthority: String?): MutableList<Url> {
         val authority = currentAuthority ?: return mutableListOf()
+        val app = file.parentEmberApp ?: return mutableListOf()
+        val appName = Ember.getAppName(app) ?: return mutableListOf()
         val module = file.parentEmberModule ?: return mutableListOf()
-        val appName = Ember.getAppName(module) ?: return mutableListOf()
 
         val path = file.path
                 .removePrefix(module.path)
