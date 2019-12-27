5719ffa8a96beb7d997ef00b2fe143cf808d53a2
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-12-27 16:30:02 +0100

    Add `EmberXmlElementDescriptorProvider` for goto component on tag names

---
+++ src/main/kotlin/com/emberjs/EmberXmlElementDescriptorProvider.kt
@@ -0,0 +1,18 @@
+package com.emberjs
+
+import com.intellij.psi.impl.source.html.HtmlFileImpl
+import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider
+import com.intellij.psi.xml.XmlTag
+import com.intellij.xml.XmlElementDescriptor
+
+class EmberXmlElementDescriptorProvider : XmlElementDescriptorProvider {
+    override fun getDescriptor(tag: XmlTag?): XmlElementDescriptor? {
+        if (tag == null) return null
+
+        val containingFile = tag.containingFile as? HtmlFileImpl ?: return null
+        val language = containingFile.contentElementType?.language ?: return null
+        if (language.id !== "Handlebars") return null
+
+        return EmberXmlElementDescriptor(tag)
+    }
+}
