c241db207d112c35bb48e6c00c015c496ec7bdcc
patrick <patrick.pircher@ibm.com>
2020-11-20 19:21:10 +0100

    implement features

--- src/main/kotlin/com/emberjs/hbs/HbsParameterInfoHandler.kt
+++ src/main/kotlin/com/emberjs/hbs/HbsParameterInfoHandler.kt
@@ -8,16 +8,21 @@ import com.emberjs.utils.resolveModifier
 import com.intellij.codeInsight.hints.InlayInfo
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.lang.javascript.psi.JSFunction
+import com.intellij.lang.javascript.psi.JSParameter
 import com.intellij.lang.javascript.psi.JSParameterListElement
+import com.intellij.lang.javascript.psi.JSRecordType
+import com.intellij.lang.javascript.psi.types.JSArrayType
+import com.intellij.lang.javascript.psi.types.JSSimpleRecordTypeImpl
 import com.intellij.lang.javascript.psi.types.JSTupleType
 import com.intellij.lang.parameterInfo.*
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.util.elementType
 import com.intellij.psi.util.parents
+import com.intellij.util.containers.toArray
 
 
-class HbsParameterInfoHandler : ParameterInfoHandler<PsiElement, JSFunction> {
+class HbsParameterInfoHandler : ParameterInfoHandler<PsiElement, JSParameter> {
     override fun couldShowInLookup(): Boolean {
         return true
     }
@@ -58,8 +63,18 @@ class HbsParameterInfoHandler : ParameterInfoHandler<PsiElement, JSFunction> {
     override fun findElementForParameterInfo(context: CreateParameterInfoContext): JSFunction? {
         val psiElement = context.file.findElementAt(context.offset)
         val func = findHelperFunction(psiElement)
-        context.itemsToShow = func?.parameters
-        return func
+        if (func != null) {
+            val modifier = resolveModifier(func.containingFile)
+            if (modifier != null) {
+                context.itemsToShow = modifier.parameters.toList().takeLast(1).toTypedArray()
+                return modifier
+            } else {
+                context.itemsToShow = func.parameters
+            }
+
+            return func
+        }
+        return null
     }
 
     override fun showParameterInfo(element: PsiElement, context: CreateParameterInfoContext) {
@@ -80,19 +95,13 @@ class HbsParameterInfoHandler : ParameterInfoHandler<PsiElement, JSFunction> {
         context.setCurrentParameter(currentParam)
     }
 
-    override fun updateUI(p: JSFunction?, context: ParameterInfoUIContext) {
+    override fun updateUI(p: JSParameter?, context: ParameterInfoUIContext) {
         var text = ""
         if (p == null) {
             return
         }
-        val modifier = resolveModifier(p.containingFile)
-        if (modifier != null) {
-            val param = p.parameters.lastOrNull()
-            param?.jsType
-        }
-        val array = p.parameters.firstOrNull()
-        val hash = p.parameters.lastOrNull()
-        if (array != null) {
+        if (p.jsType is JSArrayType || p.jsType is JSTupleType) {
+            val array = p
             val names = array.children.getOrNull(0)?.children?.map { it.text }
             val type = array.jsType
             if (type is JSTupleType) {
@@ -102,8 +111,8 @@ class HbsParameterInfoHandler : ParameterInfoHandler<PsiElement, JSFunction> {
             }
         }
 
-        if (hash != null) {
-            val type = hash.jsType
+        if (p.jsType is JSRecordType) {
+            val type = p.jsType
             if (type != null) {
                 text += type.resolvedTypeText
             }
