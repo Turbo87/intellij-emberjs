2a881462a071220b5246c34fe4a41ed394ff8881
patricklx <patricklx@users.noreply.github.com>
2020-11-20 21:26:46 +0100

    Update EmberTagNameProvider.kt

--- src/main/kotlin/com/emberjs/EmberTagNameProvider.kt
+++ src/main/kotlin/com/emberjs/EmberTagNameProvider.kt
@@ -142,44 +142,6 @@ class EmberTagNameProvider : XmlTagNameProvider {
     }
 }
 
-class PathKeyClass : Key<String>("PATH")
-val PathKey = PathKeyClass()
-
-private class HbsInsertHandler : InsertHandler<LookupElement> {
-
-    override fun handleInsert(context: InsertionContext, item: LookupElement) {
-        val path = item.getUserData(PathKey) ?: return
-        val fullName = item.lookupString.replace("::", "/")
-        val name = fullName.split("/").last()
-        val pattern = "\\{\\{\\s*import\\s+([\\w*\"']+[-,\\w*\\n'\" ]+)\\s+from\\s+['\"]([^'\"]+)['\"]\\s*\\}\\}"
-        val matches = Regex(pattern).findAll(context.document.text)
-        val importsSq = matches.map {
-            it.groups.filterNotNull().map { it.value }.toMutableList().takeLast(2).toMutableList()
-        }
-        val imports = importsSq.asIterable().toMutableList()
-        val m = imports.indexOfFirst { it.last() == path }
-
-        if (m != -1) {
-            val groups = imports.elementAt(m)
-            if (groups.first().contains(name)) return
-            val g = groups.elementAt(0).replace("'", "").replace("\"", "")
-            groups.removeAt(0)
-            groups[0] = "'$g,$name'"
-        } else {
-            val l = arrayOf(name, path!!)
-            imports.add(l.toMutableList())
-        }
-        var text = context.document.text
-        text = text.replace(Regex("$pattern.*\n"), "")
-        for (imp in imports.reversed()) {
-            val l = arrayOf("{{import", imp.elementAt(0), "from '${imp.elementAt(1)}'}}")
-            text = l.joinToString(" ") + "\n" + text
-        }
-        context.document.setText(text)
-        context.commitDocument()
-    }
-
-}
 
 fun toLookupElement(name: EmberName, priority: Double = 90.0): LookupElement {
     val lookupElement = LookupElementBuilder
@@ -188,7 +150,6 @@ fun toLookupElement(name: EmberName, priority: Double = 90.0): LookupElement {
             .withTypeText("component")
             .withIcon(EmberIconProvider.getIcon("component"))
             .withCaseSensitivity(true)
-            .withInsertHandler(HbsInsertHandler())
     lookupElement.putUserData(PathKey, name.path)
     return PrioritizedLookupElement.withPriority(lookupElement, priority)
 }
