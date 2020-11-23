10ceef9bc61c4324e94090545bdd1cb975d1ec8d
patrick <patrick.pircher@ibm.com>
2020-11-23 16:48:28 +0100

    add parameter name hints and add parameter info (ctrl+p)

---
+++ src/main/kotlin/com/emberjs/hbs/HbsParameterInfoHandler.kt
@@ -0,0 +1,119 @@
+package com.emberjs.hbs
+
+import com.dmarcotte.handlebars.psi.HbParam
+import com.emberjs.utils.findFirstHbsParamFromParam
+import com.emberjs.utils.followReferences
+import com.emberjs.utils.resolveHelper
+import com.emberjs.utils.resolveModifier
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.lang.javascript.psi.*
+import com.intellij.lang.javascript.psi.types.JSArrayType
+import com.intellij.lang.javascript.psi.types.JSTupleType
+import com.intellij.lang.parameterInfo.*
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+
+
+class HbsParameterInfoHandler : ParameterInfoHandler<PsiElement, Any?> {
+    override fun couldShowInLookup(): Boolean {
+        return true
+    }
+
+    override fun getParametersForLookup(item: LookupElement?, context: ParameterInfoContext?): Array<*>? {
+        val psiElement = context?.file?.findElementAt(context.offset)
+        val helper = findFirstHbsParamFromParam(psiElement)
+
+        val file = helper?.references?.getOrNull(0)?.resolve()?.containingFile
+        if (file == null) {
+            return null
+        }
+        return resolveHelper(file)?.parameters
+    }
+
+    override fun findElementForParameterInfo(context: CreateParameterInfoContext): PsiElement? {
+        val psiElement = context.file.findElementAt(context.offset)
+        val block = findFirstHbsParamFromParam(psiElement)
+        val ref = followReferences(block)
+        if (ref == null) {
+            return null
+        }
+        val func = resolveHelper(ref.containingFile)
+        if (func == null) {
+            val modifier = resolveModifier(ref.containingFile)
+            if (modifier.filterNotNull().isNotEmpty()) {
+                val args = emptyList<Any>().toMutableList()
+                val argType = modifier.first()?.parameters?.getOrNull(2)?.jsType
+                        ?: modifier[1]?.parameters?.getOrNull(1)?.jsType
+                        ?: modifier[2]?.parameters?.getOrNull(1)?.jsType
+                if (argType is JSRecordType) {
+                    val positional = argType.findPropertySignature("positional")
+                    if (positional != null) {
+                        args.add(positional)
+                    }
+                    val named = argType.findPropertySignature("named")
+                    if (named != null) {
+                        args.add(named)
+                    }
+                }
+                context.itemsToShow = args.toTypedArray()
+            }
+        } else {
+            context.itemsToShow = func.parameters
+        }
+        return psiElement
+    }
+
+    override fun showParameterInfo(element: PsiElement, context: CreateParameterInfoContext) {
+        context.showHint(element, element.textOffset, this)
+    }
+
+    override fun findElementForUpdatingParameterInfo(context: UpdateParameterInfoContext): PsiElement? {
+        return context.file.findElementAt(context.offset)
+    }
+
+    override fun updateParameterInfo(parameterOwner: PsiElement, context: UpdateParameterInfoContext) {
+        val psiElement = context.file.findElementAt(context.offset)
+        if (psiElement == null) {
+            return
+        }
+        val currentParam = psiElement.parent.children.filter { it is HbParam }.indexOf(psiElement) - 1
+        context.setCurrentParameter(currentParam)
+    }
+
+    override fun updateUI(p: Any?, context: ParameterInfoUIContext) {
+        var text = ""
+        if (p == null) {
+            return
+        }
+        var type: JSType? = null
+        var arrayName: String? = null
+        if (p is JSParameterListElement) {
+            arrayName = p.name
+            type = p.inferredType
+        }
+        if (p is JSRecordType.PropertySignature) {
+            arrayName = p.memberName
+            type = p.jsType
+        }
+        if (type is JSArrayType || type is JSTupleType) {
+            if (type is JSTupleType) {
+                val names = type.sourceElement?.children?.map { it.text } ?: emptyList<String>()
+                text += names.mapIndexed { index, s -> "$s:${type.getTypeByIndex(index) ?: "unknown"}" }.joinToString(",")
+            } else {
+                val arrayType = type as JSArrayType
+                text += arrayName + ":" + (arrayType.type?.resolvedTypeText ?: "*")
+            }
+        }
+
+        if (type is JSRecordType) {
+            text += type.properties.map { it.memberName + ":" + it.jsType?.resolvedTypeText + "=" }.joinToString(",")
+        }
+
+
+        if (text == "") {
+            return
+        }
+        context.setupUIComponentPresentation(text, 0, 0, false, false, false, context.defaultParameterColor)
+    }
+
+}
---
+++ src/main/kotlin/com/emberjs/hbs/HbsParameterNameHints.kt
@@ -0,0 +1,99 @@
+package com.emberjs.hbs
+
+import com.dmarcotte.handlebars.psi.HbMustacheName
+import com.dmarcotte.handlebars.psi.HbParam
+import com.emberjs.utils.findFirstHbsParamFromParam
+import com.emberjs.utils.followReferences
+import com.emberjs.utils.resolveHelper
+import com.emberjs.utils.resolveModifier
+import com.intellij.codeInsight.hints.HintInfo
+import com.intellij.codeInsight.hints.InlayInfo
+import com.intellij.codeInsight.hints.InlayParameterHintsProvider
+import com.intellij.codeInsight.hints.Option
+import com.intellij.lang.javascript.psi.*
+import com.intellij.lang.javascript.psi.types.JSTupleType
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.refactoring.suggested.startOffset
+import java.lang.Integer.max
+import java.util.*
+
+
+class HbsParameterNameHints : InlayParameterHintsProvider  {
+
+    override fun getParameterHints(psiElement: PsiElement): MutableList<InlayInfo> {
+        if (psiElement is HbParam) {
+            val firstParam = findFirstHbsParamFromParam(psiElement)
+            if (firstParam == null) {
+                return emptyList<InlayInfo>().toMutableList()
+            }
+            var index = max(
+                    firstParam.parent.children.filter { it is HbParam }.indexOf(psiElement),
+                    firstParam.parent.parent.children.filter { it is HbParam }.indexOf(psiElement)
+            )
+            if (index <= 0 && firstParam !is HbMustacheName) {
+                // if its the helper itself
+                return emptyList<InlayInfo>().toMutableList()
+            } else {
+                index += 1
+            }
+
+
+            var file = followReferences(firstParam)
+            if (file == firstParam) {
+                file = followReferences(firstParam)
+            }
+
+            if (file != null) {
+                var func = if (file is PsiFile) resolveHelper(file) else if (file is JSFunction) file else null
+        var arrayName: String? = null
+        var array: JSType? =  null
+
+        if (func != null) {
+            arrayName = func.parameters.first().name ?: arrayName
+            array = func.parameters.first().jsType
+        } else {
+            val modifier = resolveModifier(file.containingFile)
+            val args = modifier.first()?.parameters?.getOrNull(2)?.jsType
+                    ?: modifier[1]?.parameters?.getOrNull(1)?.jsType
+                    ?: modifier[2]?.parameters?.getOrNull(1)?.jsType
+            array = null
+            if (args is JSRecordType) {
+                array = args.findPropertySignature("positional")?.jsType
+                arrayName = "positional"
+            }
+        }
+
+        val type = array
+        if (type is JSTupleType && type.sourceElement != null) {
+            val name = type.sourceElement!!.children.map { it.text }.getOrNull(index-1)
+            if (name != null) {
+                        return mutableListOf(InlayInfo(name, psiElement.startOffset))
+            } else {
+                if (index == 1 && arrayName != null) {
+                    return mutableListOf(InlayInfo(arrayName, psiElement.startOffset))
+                }
+            }
+
+            }
+            }
+        }
+        return emptyList<InlayInfo>().toMutableList()
+    }
+
+    override fun getHintInfo(element: PsiElement): HintInfo? {
+        return null
+    }
+
+    override fun getDefaultBlackList(): MutableSet<String> {
+        return Collections.emptySet()
+    }
+
+    override fun getSupportedOptions(): List<Option?> {
+        return Collections.emptyList()
+    }
+
+    override fun isBlackListSupported(): Boolean {
+        return false
+    }
+}
