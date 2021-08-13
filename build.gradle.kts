1be54c59461c40bb01db9e4f7b94fdf756de88cd
Andrey Stukalin <apstukalin@gmail.com>
2021-08-13 08:42:40 +0300

    remove commented build code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key).toString()
@@ -57,38 +56,7 @@ tasks {
         kotlinOptions.jvmTarget = "1.8"
     }
 
-//    patchPluginXml {
-//        version.set(properties("pluginVersion"))
-//        sinceBuild.set(properties("pluginSinceBuild"))
-//        untilBuild.set(properties("pluginUntilBuild"))
-//
-//        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-////        pluginDescription.set(
-////            File(projectDir, "README.md").readText().lines().run {
-////                val start = "<!-- Plugin description -->"
-////                val end = "<!-- Plugin description end -->"
-////
-////                if (!containsAll(listOf(start, end))) {
-////                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-////                }
-////                subList(indexOf(start) + 1, indexOf(end))
-////            }.joinToString("\n").run { markdownToHTML(this) }
-////        )
-//
-//        // Get the latest available change notes from the changelog file
-//        changeNotes.set(provider { changelog.getLatest().toHTML() })
-//    }
-
-//    runPluginVerifier {
-//        ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
-//    }
-
     publishPlugin {
-//        dependsOn("patchChangelog")
         token.set(System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken"))
-        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-//        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
 }
