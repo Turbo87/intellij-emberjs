4a7e19eaa608ff3d629ccce6e4f27f56063bb0f2
Tobias Bieniek <tobias@bieniek.cloud>
2022-12-01 10:27:17 +0100

    Gradle: Update JVM target versions
    This addresses a build warning from the IntelliJ plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,13 +43,12 @@ intellij {
 }
 
 tasks {
-    // Set the compatibility versions to 1.8
     withType<JavaCompile> {
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "17"
     }
 
     publishPlugin {
